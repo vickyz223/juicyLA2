feb40cd5a59a07b930ec57d69fd22ba3f41df445
Ryan Yeo <ryandc.yeo@gmail.com>
2022-05-20 16:47:59 -0700

    commented out Register and Login imports from index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 import NavBar from './NavBar';
 import Homepage from './Homepage';
-import Register from './Register';
-import Login from './Login';
+// import Register from './Register';
+// import Login from './Login';
 import RestaurantPage from './RestaurantPage';
 
 function JuicyLa() {
     return (
         <div>
-            <Router>            
-                <NavBar/>
-            <Routes>
-                <Route path="/Register" element={<Register/>}/>
-                <Route path="/Login" element={<Login/>}/>
-                <Route path = "/RestaurantPage" element={<RestaurantPage/>}/>
-                <Route path="/" element={<Homepage/>}/>
-            </Routes>
+            <Router>
+                <NavBar />
+                <Routes>
+                    {/* <Route path="/Register" element={<Register/>}/>
+                <Route path="/Login" element={<Login/>}/> */}
+                    <Route path="/RestaurantPage" element={<RestaurantPage />} />
+                    <Route path="/" element={<Homepage />} />
+                </Routes>
 
             </Router>
         </div>
