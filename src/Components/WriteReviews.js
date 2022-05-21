a4b1c19ecaf9161eba209608a00ad7382a6d905b
Ryan Yeo <ryandc.yeo@gmail.com>
2022-05-21 16:54:22 -0700

    fixed merge issue

--- src/Components/WriteReviews.js
+++ src/Components/WriteReviews.js
@@ -4,8 +4,30 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Rating from '@mui/material/Rating';
 import './WriteReviews.css';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABvCRnBiC8FAlfOOY4WLGoEvtAQM4Jzsw",
+  authDomain: "juicyla-2022.firebaseapp.com",
+  databaseURL: "https://juicyla-2022-default-rtdb.firebaseio.com/",
+  projectId: "juicyla-2022",
+  storageBucket: "juicyla-2022.appspot.com",
+  messagingSenderId: "132540762702",
+  appId: "1:132540762702:web:77cbc2746dcdee081bd11f",
+  measurementId: "G-32ZW6T1JQR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
 function writeUserData(diningId, username, review, stars) {
   const db = getDatabase();
   const reference = ref(db, 'written reviews' + '/' + diningId + '/' + username);
