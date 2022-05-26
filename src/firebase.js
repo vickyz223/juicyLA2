318b4530f3d05ae4687090a839475e11d975f29e
Gene Jeoung <39165323+jeounggene@users.noreply.github.com>
2022-05-26 15:29:13 -0700

    picture change

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-// 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-// 	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-// 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-// 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESS_SENDER_ID,
-// 	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// 	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyABvCRnBiC8FAlfOOY4WLGoEvtAQM4Jzsw",
   authDomain: "juicyla-2022.firebaseapp.com",
