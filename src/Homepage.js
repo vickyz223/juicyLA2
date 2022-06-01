261a8b7e4031859307c5927da375dbb21bdd41c0
lilyZhou <lzhou0714@g.ucla.edu>
2022-06-01 02:45:42 -0700

    Working star, menu has issues

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,85 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './Homepage.css';
 import PodiumBox from './PodiumBox';
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, get, child} from "firebase/database";
 
 
-function getActivity(diningId) {
-
+const getRating = async (diningId) => {
     const db = getDatabase();
-    const reference = ref(db, 'activity' + '/' + diningId);
-
-    let activity;
-    onValue(reference, (snapshot) => {
-        activity = snapshot.val();
-    });
-    return (activity);
-}
-
-const getRating = (diningId) => {
-    const db = getDatabase();
-    const reference = ref(db, 'ratings' + '/' + diningId);
-
-    let num;
-    onValue(reference, (snapshot) => {
-        num = snapshot.val().rating;
-    });
-    return (num);
+    const num = (await get(child(ref(db),'ratings' + '/' + diningId))).val().rating;
+    return num;
 };
 
-function getMenu(diningId) {
-    const db = getDatabase();
-    const reference = ref(db, 'menu' + '/' + diningId);
 
-    let menu;
-    onValue(reference, (snapshot) => {
-        menu = snapshot.val();
-    });
-    return (menu);
-
-}
-
-function diningHall(name, rating, menu, activity) {
+function diningHall(name, rating) {
     this.name = name;
     this.rating = rating;
-    this.menu = menu;
-    this.activity = activity;
 }
 
 const Homepage = () => {
     let navigate = useNavigate();
     const places = ['Epicuria', 'BruinPlate', 'DeNeve'];
     let len = places.length;
-    
-    const restaurants = [];
 
-    for (let i = 0; i < len; i++) {
-        const name = places[i];
-        const item = new diningHall(name, getRating(name), getMenu(name), getActivity(name));
-        if(i == 0) {
-            restaurants.push(item);
-        } else if(i == 1) {
-            if(restaurants[0].rating > getRating(name)) {
-                restaurants.push(item);
+    const [restaurants, setRestaurants] = useState(null);
+
+    useEffect(() => {
+        async function getRestaurantData () {
+            const restaurantTemp = []
+            for (let i = 0; i < len; i++) {
+                const name = places[i];
+                const item = new diningHall(name, await getRating(name));
+                restaurantTemp.push(item);
             }
-            else {
-                restaurants.unshift(item);
-            }
-        } else {
-            if(restaurants[0].rating < getRating(name)) {
-                restaurants.unshift(item);
-            } else if(restaurants[1].rating < getRating(name)) {
-                restaurants.splice(1,0,item);
-            } else {
-                restaurants.push(item);
-            }
+            restaurantTemp.sort((a, b) => {
+                if (a.rating > b.rating) {
+                    return -1;
+                } else if (a.rating < b.rating) {
+                    return 1;
+                }
+                return 0;
+            });
+            setRestaurants(restaurantTemp);
         }
-    }
+        getRestaurantData();
+    }, [])
 
     // const [name, setName] = useState('Name');
     return (
-        <>
+        restaurants && 
+        (<>
             <div className='topDivider'>
                 <div className='brand'>
                     JUICYLA
@@ -124,7 +93,7 @@ const Homepage = () => {
                 <h1>fyghj</h1>
                 <h1>fyghj</h1>
             </div> */}
-        </>
+        </>)
     );
 }
 
