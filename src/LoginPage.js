4763ed6f9ac33dd02a4b5487971331f17f592821
Gene Jeoung <39165323+jeounggene@users.noreply.github.com>
2022-06-01 17:18:07 -0700

    finished styling nav and login and signup

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,9 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert, Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "./UserAuthContext";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './LoginPage.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./LoginPage.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -36,47 +35,47 @@ const Login = () => {
   };
 
   return (
-	<div className="background">
-		<div className="loginDiv">
-		<div className="p-2 box">
-			<h2 className="mb-3">Firebase Auth Login</h2>
-			{error && <Alert variant="danger">{error}</Alert>}
-			<Form onSubmit={handleSubmit}>
-			<Form.Group className="mb-3" controlId="formBasicEmail">
-				<Form.Control
-				type="email"
-				placeholder="Email address"
-				onChange={(e) => setEmail(e.target.value)}
-				/>
-			</Form.Group>
+    <div className="background">
+      <div className="loginDiv">
+        <div className="p-2 box">
+          <h2 className="mb-3 textShadow">JUICYLA Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-			<Form.Group className="mb-3" controlId="formBasicPassword">
-				<Form.Control
-				type="password"
-				placeholder="Password"
-				onChange={(e) => setPassword(e.target.value)}
-				/>
-			</Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-			<div className="d-grid gap-2">
-				<Button variant="primary" type="Submit">
-				Log In
-				</Button>
-			</div>
-			</Form>
-			<hr className="loginHr"/>
-			<div className="d-flex justify-content-center">
-				<GoogleButton
-					type="dark"
-					onClick={handleGoogleSignIn}
-				/>
-			</div>
-		</div>
-		<div className="box text-center textShadow">
-			No account? <Link to="/Signup">Sign up</Link>
-		</div>
-		</div>
-	</div>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="Submit">
+                Log In
+              </Button>
+            </div>
+          </Form>
+          <hr className="loginHr" />
+          <div className="d-flex justify-content-center">
+            <GoogleButton type="dark" onClick={handleGoogleSignIn} />
+          </div>
+        </div>
+        <div className="box text-center textShadow">
+          No account?{" "}
+          <Link className="noTxtDec" to="/Signup">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
