00b86d122acce87ece53d36041ee63bffe7ece4a
vicky <vzhong2021@gmail.com>
2023-01-12 12:21:45 -0800

    fix: Fixes CSS for mobile displays for navbar, homepage, and podium

--- src/PodiumBox.js
+++ src/PodiumBox.js
@@ -8,18 +8,14 @@ import './Homepage.css';
 
 const PodiumBox = ({ rating, name }) => {
     return (
-      <div>
+      <div className="podiumHolder">
         <div className="podiumName">{name}</div>
-        <div className="info">
-          <>
-            <div className="stars">
-              <Rating value={rating} readOnly />
-            </div>
-            <div className="activity">
-              <div> Activity</div>
-            </div>
-            <ActivityLevel restaurant={name} />
-          </>
+        <div className="stars">
+          <Rating value={rating} readOnly />
+        </div>
+        <div className="activity">
+          <div> Capacity: </div>
+          <ActivityLevel restaurant={name} />
         </div>
         <div className="summary">
           <MenuComponent restaurant={name} />
